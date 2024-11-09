ae243fcde4efcfded43d5b179c174141211da4ff
serii burduja <seriiburduja@gmail.com>
2024-11-09 10:08:59 +0200

    feat: home about and contact

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+@section('content')
+<header class="py-5 bg-light border-bottom mb-4">
+    <div class="container">
+        <div class="text-center my-5">
+            <h1 class="fw-bolder">Contact us</h1>
+        </div>
+    </div>
+</header>
+<!-- Page content-->
+<div class="container">
+    <div class="row">
+        <!-- Blog entries-->
+        <div class="col-lg-12">
+            <p>Contact us page</p>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('index');
+})->name('home');
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
